8eed1f04a2e83a89adcad13797eaad1b9153c19f
BillyGalbreath <Blake.Galbreath@GMail.com>
2017-12-29 00:00:50 -0600

    Fix capabilities

--- src/main/java/net/pl3x/bones/capability/CapabilityHandler.java
+++ src/main/java/net/pl3x/bones/capability/CapabilityHandler.java
@@ -1,10 +1,9 @@
 package net.pl3x.bones.capability;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.pl3x.bones.Bones;
 
@@ -13,17 +12,8 @@ public class CapabilityHandler {
 
     @SubscribeEvent
     public void on(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof EntityPlayer) {
+        if (event.getObject() instanceof EntitySkeleton) {
             event.addCapability(DECEPTION_TARGET, new DeceptionTarget.Provider());
-        }
-    }
-
-    @SubscribeEvent
-    public void on(PlayerEvent.Clone event) {
-        DeceptionTarget newData = getCapability(event.getEntityPlayer());
-        DeceptionTarget oldData = getCapability(event.getOriginal());
-        if (newData != null && oldData != null) {
-            newData.setDataFromNBT(oldData.getDataAsNBT());
         }
     }
 
